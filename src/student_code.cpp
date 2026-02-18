be104585e0d9ed61582390d704032c4c4a885002
ansawaisai-beep <ansawaisai@gmail.com>
2026-02-18 14:54:48 +0500

    Update student_code.cpp

--- src/student_code.cpp
+++ src/student_code.cpp
@@ -18,7 +18,7 @@ float getFloatValue() {
 char getCharValue() {
     // Declare a variable var_char of type character and set its value to A.
     //return var_char;
-    char var_char = A;
+    char var_char = 'A';
     return var_char;
 }
 
